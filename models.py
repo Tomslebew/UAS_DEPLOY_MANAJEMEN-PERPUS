f7110a2cac5aaebe610897cbec9d0703ec206fd6
Tomslebew <tomiilyas2005@gmail.com>
2025-01-23 17:26:33 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# Inisialisasi SQLAlchemy
 db = SQLAlchemy()
 
+# Definisi Model Buku
 class Book(db.Model):
+    __tablename__ = 'books'  # Nama tabel dalam database
     id = db.Column(db.Integer, primary_key=True)
-    judul = db.Column(db.String(100), nullable=False)
-    pengarang = db.Column(db.String(100), nullable=False)
+    judul = db.Column(db.String(100), nullable=False, index=True)
+    pengarang = db.Column(db.String(100), nullable=False, index=True)
     tahun = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
